f530766686308e8b9361451980ca738265d976f9
Mubarak Yahaya <Mubrikdev@gmail.com>
2021-03-28 16:39:31 +0100

    allowed hosts

--- alphanote/settings.py
+++ alphanote/settings.py
@@ -160,7 +160,7 @@ SECRET_KEY = os.environ.get('SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['https://aplhanoteapp-django.herokuapp.com', '127.0.0.1']
+ALLOWED_HOSTS = ['aplhanoteapp-django.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
