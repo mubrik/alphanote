4488962598acece55e9720124a7379134b902282
Mubarak Yahaya <Mubrikdev@gmail.com>
2021-04-02 19:11:04 +0100

    moved linux get to settings scripts

--- alphanote/settings.py
+++ alphanote/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
+from .settings_scripts import get_linux_ec2_private_ip
 
 # my custom settings 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -38,7 +39,7 @@ AWS_S3_OBJECT_PARAMETERS = {
 
 # whitenoise
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
-STATIC_HOST = os.environ['STATIC_HOST'] if not DEBUG else ''
+STATIC_HOST = os.environ['STATIC_HOST'] 
 STATIC_URL = STATIC_HOST + '/static/' 
 
 # Database postgres
@@ -169,24 +170,9 @@ ALLOWED_HOSTS = [
 ]
 
 # adds aws elb private ip to allowed hosts
-def get_linux_ec2_private_ip():
-    """Get the private IP Address of the machine if running on an EC2 linux server"""
-    from urllib.request import urlopen
-    try:
-        response = urlopen('http://169.254.169.254/latest/meta-data/local-ipv4')
-        return response.read().decode("utf-8")
-    except:
-        return None
-    finally:
-        if response:
-            response.close()
-# ElasticBeanstalk healthcheck sends requests with host header = internal ip
-# So we detect if we are in elastic beanstalk,
-# and add the instances private ip address
-if not DEBUG:
-    private_ip = get_linux_ec2_private_ip()
-    if private_ip:
-        ALLOWED_HOSTS.append(private_ip)
+private_ip = get_linux_ec2_private_ip()
+if private_ip:
+    ALLOWED_HOSTS.append(private_ip)
 
 
 # Application definition
