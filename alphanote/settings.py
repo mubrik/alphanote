fd19e3154201734dd63ebeea019a952d08316c85
Mubarak Yahaya <Mubrikdev@gmail.com>
2021-04-02 18:51:23 +0100

    testing 1

--- alphanote/settings.py
+++ alphanote/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 
 import os
 from pathlib import Path
-from .settings_scripts import get_linux_ec2_private_ip
 
 # my custom settings 
 # SECURITY WARNING: don't run with debug turned on in production!
@@ -168,6 +167,22 @@ ALLOWED_HOSTS = [
     'localhost', '.amazonaws.com',
     '.elasticbeanstalk.com', '127.0.0.1',
 ]
+
+# adds aws elb private ip to allowed hosts
+def get_linux_ec2_private_ip():
+    """Get the private IP Address of the machine if running on an EC2 linux server"""
+    from urllib.request import urlopen
+    try:
+        response = urlopen('http://169.254.169.254/latest/meta-data/local-ipv4')
+        return response.read().decode("utf-8")
+    except:
+        return None
+    finally:
+        if response:
+            response.close()
+# ElasticBeanstalk healthcheck sends requests with host header = internal ip
+# So we detect if we are in elastic beanstalk,
+# and add the instances private ip address
 
 private_ip = get_linux_ec2_private_ip()
 if private_ip:
