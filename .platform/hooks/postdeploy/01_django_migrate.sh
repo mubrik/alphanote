ab9b4eee3af2bbe5c44fbc3cdb6844597c4b18c0
Mubarak Yahaya <Mubrikdev@gmail.com>
2021-04-08 13:34:05 +0100

    test2

--- .platform/hooks/postdeploy/01_django_migrate.sh
+++ .platform/hooks/postdeploy/01_django_migrate.sh
@@ -1,8 +1,1 @@
 #!/bin/sh
-source /var/app/venv/*/bin/activate
-cd /var/app/staging
-
-python manage.py showmigrations;
-python manage.py migrate --noinput;
-python manage.py collectstatic --noinput;
-python manage.py createsu;
