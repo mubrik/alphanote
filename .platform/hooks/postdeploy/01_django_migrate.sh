e6e5afece007a270493ef75f17e567b776612039
Mubarak Yahaya <Mubrikdev@gmail.com>
2021-04-08 22:12:42 +0100

    added all commands to singe script

--- .platform/hooks/postdeploy/01_django_migrate.sh
+++ .platform/hooks/postdeploy/01_django_migrate.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-source /var/app/venv/*/bin/activate
-cd /var/app/staging
+TASKS_DIR=/opt/elasticbeanstalk/tasks
 
-python manage.py makemigrations
-python manage.py migrate
-python manage.py createsu
-python manage.py collectstatic --noinput
+# including django log in bundle logs
+# log file in env var
+echo "${LOG_FILE_PATH//.log/*}" > "$TASKS_DIR/bundlelogs.d/01-app-log.conf"
+# include current app log file in tail logs
+echo $LOG_FILE_PATH > "$TASKS_DIR/taillogs.d/01-app-log.conf"
+
+source "$PYTHONPATH/activate" && {
+# log which migrations have already been applied
+python manage.py showmigrations;
+# migrate
+python manage.py migrate --noinput;
+
+# create super user
+python manage.py createsu;
+
+# collectstatic
+python manage.py collectstatic --noinput;
+}
